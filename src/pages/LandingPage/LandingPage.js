2203574587b8acc91721293bcd469e20c1be98bf
Nicolas Venzmer <nicovenz@hotmail.com>
2020-09-12 11:15:53 -0300

    Correcion de la Landing Page.

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import imagen2 from '../../imagenes/Imagen2.png';
+import imagen1 from '../../imagenes/imagen1.png';
 import InputIcon from '@material-ui/icons/Input';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import IconButton from "@material-ui/core/IconButton";
- 
+import "./LandingPage.css";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
@@ -14,34 +15,26 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 }));
- 
+
+
 export default function () {
   const classes = useStyles();
   return (
-    <div
-      style={{
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        bottom: 0,
-        right: 0,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-      }}
-    >
-        <img src ={imagen2} width="100%" alt="Imagen Benchmarking"/>
-        <center><h1>Bienvenido al Termometro Pyme!</h1></center>
-        <IconButton edge="end" className={classes.InputIcon} color="inherit"  aria-label="menu">
-        <Button 
-          variant="contained"
-          color="inherit"
-          className={classes.button}
-          startIcon={<InputIcon />}
-          >
-          <Link to="/home"><center>Ingresar</center></Link></Button>
-      </IconButton>
+    <div id="container">
+      <img id="image" src ={imagen1} width="100%" height="100%" alt="Imagen Benchmarking" />
+      <p id="text">
+      <center>Bienvenido al Termometro Pyme!</center>
+      <IconButton edge="end" className={classes.InputIcon} color="inherit"  aria-label="menu">
+          <Button 
+            variant="contained"
+            color="inherit"
+            className={classes.button}
+            startIcon={<InputIcon />}
+            >
+            <Link to="/home"><center>Ingresar</center></Link>
+          </Button>
+        </IconButton>
+      </p>
     </div>
   )
 }
