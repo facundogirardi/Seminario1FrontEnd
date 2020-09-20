b32f97d0efa234ce608fcae8ee7eec74993f0700
Nicolas Venzmer <nicovenz@hotmail.com>
2020-09-20 19:48:14 -0300

    Correccion de footer.

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import "./Footer.css";
+import logo from './continuar.jpg';
+import IconButton from "@material-ui/core/IconButton";
+import InstagramIcon from '@material-ui/icons/Instagram';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 function Footer() {
+  const classes = useStyles();
   return (
     <div className="main-footer">
-      <p className="col-sm">
-          <center>© FUNDACIÓN OBSERVATORIO PYME © 2014. Todos los derechos reservados. TEL 5411 4381 3331 Av. de Mayo 1147, Piso 3. Buenos Aires. Argentina</center>  
-      </p>
-    </div>
+        <div id="izquierda">
+          <center><img src={logo} width="70px" alt="Logo"/></center>
+          <div id="derecha">
+          <center>
+          <IconButton edge="start" className={classes.InstagramIcon} color="primary"  aria-label="menu" href="https://www.instagram.com/observatoriopyme/">
+            <InstagramIcon/>
+          </IconButton>
+          <IconButton edge="start" className={classes.LinkedInIcon} color="primary"  aria-label="menu" href="https://www.linkedin.com/in/observatoriopyme/">
+            <LinkedInIcon />
+          </IconButton>
+          <IconButton edge="start" className={classes.FacebookIcon} color="primary" aria-label="menu" href="https://www.facebook.com/fundacionobservatoriopyme">
+            <FacebookIcon />
+          </IconButton>
+          <IconButton edge="start" className={classes.TwitterIcon} color="primary" aria-label="menu" href="https://twitter.com/FOPyME">
+            <TwitterIcon />
+          </IconButton>
+          </center>
+          </div>
+        </div>
+          <p>
+            <center>© FUNDACIÓN OBSERVATORIO PYME © 2020. Todos los derechos reservados. TEL +54 11 4381-3331 Av. de Mayo 1147, Piso 3. Buenos Aires. Argentina</center>  
+            <br/>
+          </p>
+      </div>
   );
 }
 
