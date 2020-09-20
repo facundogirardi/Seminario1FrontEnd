7dc934e5353e3e1fd83e89ad0cacb572cee3dc4e
Nicolas Venzmer <nicovenz@hotmail.com>
2020-09-20 12:41:59 -0300

    Agregado del Contacto en el menu y el Dashboard en los resultados.

--- src/config/menuItems.js
+++ src/config/menuItems.js
@@ -1,11 +1,8 @@
-import allLocales from './locales'
-// import allThemes from './themes'
 import React from 'react'
 import DaschboardIcon from '@material-ui/icons/Dashboard'
 import LockIcon from '@material-ui/icons/Lock'
 import SettingsIcon from '@material-ui/icons/Settings';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp'
-import LanguageIcon from '@material-ui/icons/Language'
 import MenuOpenIcon from '@material-ui/icons/MenuOpen'
 import GetApp from '@material-ui/icons/GetApp'
 import ChromeReaderMode from '@material-ui/icons/ChromeReaderMode'
@@ -16,13 +13,11 @@ import PollIcon from '@material-ui/icons/Poll';
 import InfoIcon from '@material-ui/icons/Info';
 import CreateIcon from '@material-ui/icons/Create';
 import HomeIcon from '@material-ui/icons/Home';
+import ContactsIcon from '@material-ui/icons/Contacts';
 
 const getMenuItems = (props) => {
   const {
-    appConfig,
     intl,
-    updateLocale,
-    locale,
     menuContext,
     themeContext,
     a2HSContext,
@@ -32,18 +27,6 @@ const getMenuItems = (props) => {
   const { themeID, setThemeID } = themeContext
   const { auth, setAuth } = authData
   const { isAppInstallable, isAppInstalled, deferredPrompt } = a2HSContext
-
-  const localeItems = allLocales.map((l) => {
-    return {
-      value: undefined,
-      visible: true,
-      primaryText: intl.formatMessage({ id: l.locale }),
-      onClick: () => {
-        updateLocale(l.locale)
-      },
-      leftIcon: <LanguageIcon />,
-    }
-  })
 
   const isAuthorised = auth.isAuthenticated
 
@@ -66,6 +49,12 @@ const getMenuItems = (props) => {
         visible: true,
         primaryText: intl.formatMessage({ id: 'Home' }),
         leftIcon: <HomeIcon />,
+        },
+        {
+        value: 'https://www.observatoriopyme.org.ar/newsite/contacto/',
+        visible: true,
+        primaryText: intl.formatMessage({ id: 'Contacto' }),
+        leftIcon: <ContactsIcon />,
         },
         {
           value: '/ABM',
@@ -163,6 +152,12 @@ const getMenuItems = (props) => {
         primaryText: intl.formatMessage({ id: 'Home' }),
         leftIcon: <HomeIcon />,
         },
+        {
+          value: 'https://www.observatoriopyme.org.ar/newsite/contacto/',
+          visible: true,
+          primaryText: intl.formatMessage({ id: 'Contacto' }),
+          leftIcon: <ContactsIcon />,
+          },
       {
         value: '/ABM',
         visible: isAuthorised,
--- src/pages/Resultados/Resultado.js
+++ src/pages/Resultados/Resultado.js
@@ -1,106 +1,104 @@
-import React from 'react';
-import "./Resultado.css";
-import { makeStyles } from '@material-ui/core/styles';
-import IconButton from "@material-ui/core/IconButton";
-import Button from '@material-ui/core/Button';
-import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
-import Scrollbar from 'material-ui-shell/lib/components/Scrollbar/Scrollbar'
-import { useIntl } from 'react-intl'
+import * as React from 'react';
 import Page from 'material-ui-shell/lib/containers/Page/Page'
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import Scrollbar from 'material-ui-shell/lib/components/Scrollbar/Scrollbar'
+import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import { Link } from 'react-router-dom'
+import {
+  Chart,
+  Title,
+  Legend,
+  ArgumentAxis,
+  BarSeries,
+  ValueAxis,
+} from '@devexpress/dx-react-chart-material-ui';
+import { energyConsumption as data } from '../Resultados/DatosTabla';
+import { Stack, Animation } from '@devexpress/dx-react-chart';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
+const legendStyles = () => ({
+  root: {
+    display: 'flex',
+    margin: 'auto',
+    flexDirection: 'row',
   },
 });
+const legendRootBase = ({ classes, ...restProps }) => (
+  <Legend.Root {...restProps} className={classes.root} />
+);
+const Root = withStyles(legendStyles, { name: 'LegendRoot' })(legendRootBase);
+const legendLabelStyles = () => ({
+  label: {
+    whiteSpace: 'nowrap',
+  },
+});
+const legendLabelBase = ({ classes, ...restProps }) => (
+  <Legend.Label className={classes.label} {...restProps} />
+);
+const Label = withStyles(legendLabelStyles, { name: 'LegendLabel' })(legendLabelBase);
 
-function createData(Compañia, Tipo, FacturacionPromedio, CarteraDePedidos) {
-  return { Compañia, Tipo, FacturacionPromedio, CarteraDePedidos};
-}
 
-const rows = [
-  createData('Coca Cola', "Grande", "6%", "No vario"),
-  createData('Pepsi', "Grande", "-3%", "Disminuyo"),
-  createData('Apple', "Grande", "13%", "Aumento"),
-  createData('Microsoft', "Grande", "2%", "No vario"),
-  createData('McDonalds', "Grande", "-4%", "Disminuyo"),
-];
+export default class Demo extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-export default function BasicTextFields() {
-    const classes = useStyles();
-    const intl = useIntl()
-    
- 
-  return (
-    <Page pageTitle={intl.formatMessage({ id: 'Usted esta en la ventana de los resultados.' })}>
+    this.state = {
+      data,
+    };
+  }
+
+  render() {
+    const { data: chartData } = this.state;
+
+    return (
+      <Page pageTitle={'Usted esta en la ventana de los resultados.'}>
       <Scrollbar
         style={{ height: '100%', width: '100%', display: 'flex', flex: 1 }}
       >
-        {intl.formatMessage({ id: ' ' })}
-        <br />
-        <div class="Cuadrado">
-          <h1> Resultados de su consulta :</h1>
-        </div>
-        <div class="BotonAtras">
-        <Link to="/Encuesta">
-          <IconButton edge="end" className={classes.ArrowBackIcon} color="inherit"  aria-label="menu">
-            <Button 
-              variant="contained"
-              color="primary"
-              className={classes.button}
-              startIcon={<ArrowBackIcon />}
-              >
-              Atras
-            </Button>
-          </IconButton>
-        </Link>
-        </div>
-        <div class="BotonGenerar">
-          <IconButton edge="end" className={classes.PictureAsPdfIcon} color="inherit"  aria-label="menu">
-            <Button 
-                variant="contained"
-                color="primary"
-                className={classes.button}
-                startIcon={<PictureAsPdfIcon />}>
-                Generar PDF
-            </Button>
-          </IconButton>
-        </div>
-          <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                <TableCell align="center">Compañia</TableCell>
-                  <TableCell align="center">Tipo</TableCell>
-                  <TableCell align="center">FacturacionPromedio</TableCell>
-                  <TableCell align="center">CarteraDePedidos</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <TableRow key={row.Compañia}>
-                    <TableCell component="th" scope="row">
-                      <center>{row.Compañia}</center>
-                    </TableCell>
-                    <TableCell align="center">{row.Tipo}</TableCell>
-                    <TableCell align="center">{row.FacturacionPromedio}</TableCell>
-                    <TableCell align="center">{row.CarteraDePedidos}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-      </Scrollbar>
+      <Paper>
+        <Chart
+          data={chartData}
+        >
+          <ArgumentAxis />
+          <ValueAxis
+            max={10000}
+          />
+
+          <BarSeries
+            name="Pepsi"
+            valueField="hydro"
+            argumentField="mes"
+          />
+          <BarSeries
+            name="Ferrari"
+            valueField="oil"
+            argumentField="mes"
+          />
+          <BarSeries
+            name="Ford"
+            valueField="gas"
+            argumentField="mes"
+          />
+          <BarSeries
+            name="Coca Cola"
+            valueField="coal"
+            argumentField="mes"
+          />
+          <BarSeries
+            name="Nissan"
+            valueField="nuclear"
+            argumentField="mes"
+          />
+          <Animation />
+          <Legend position="bottom" rootComponent={Root} labelComponent={Label} />
+          <Title text="Dashboard sobre como esta tu Pyme dentro del Rubro." />
+          <Stack
+            stacks={[
+              { series: ['Pepsi', 'Ferrari', 'Ford', 'Coca-Cola', 'Nissan'] },
+            ]}
+          />
+        </Chart>
+      </Paper>
+    </Scrollbar>
     </Page>
-    
-  );
+    );
+  }
 }
