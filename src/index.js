7b82075d5af34259a6cf8d296004f223c4808624
unknown <facundogirardi@gmail.com>
2020-09-25 18:06:03 -0300

    Fix explorer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 
 render(<App />, document.getElementById('root'))
