48f658987311fcf443608328a79ef94d090d06cb
Nicolas Venzmer <nicovenz@hotmail.com>
2020-09-30 11:58:51 -0300

    Update de la LandingPage.

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import fondo from '../../imagenes/fondo.png';
+import fondo from '../../imagenes/fondo1.jpg';
+import logo from '../../imagenes/logo.png';
+import fotoDashboard from '../../imagenes/header-teamwork.png';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import "./LandingPage.css";
@@ -9,8 +11,7 @@ import InstagramIcon from '@material-ui/icons/Instagram';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import TwitterIcon from '@material-ui/icons/Twitter';
-import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
-import BusinessIcon from '@material-ui/icons/Business';
+import LockIcon from '@material-ui/icons/Lock';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,35 +26,37 @@ const useStyles = makeStyles((theme) => ({
 export default function () {
   const classes = useStyles();
   return (
+    
     <div id="container">
-      <img src ={fondo} width="100%" height="100%" alt="Imagen Benchmarking" />
-      <p id="text">
-      <center>Bienvenido al Termometro Pyme!</center>
+      <img src ={fondo} width="100%" height="100%" alt="Imagen Benchmarking" color= "blue"/>
+      <div STYLE="position:absolute; top:200px; left:800px; visibility:visible z-index:1">
+        <img src={fotoDashboard} alt="alternative"/>
+      </div>
+      <div STYLE="position:absolute; top:10px; left:50px; visibility:visible z-index:1">
+        <img src={logo} alt="alternative"/>
+      </div>
+      <div STYLE="position:absolute; top:200px; left:50px; visibility:visible z-index:1">
+        <h1>Bienvendio al Termometro PyME!</h1>
+      </div>
+      <div STYLE="position:absolute; top:250px; left:55px; visibility:visible z-index:1">
+        <p>Conoce el estado de tu empresa en relacion al sector.</p>
+      </div>
+      <div STYLE="position:absolute; top:150px; left:990px; visibility:visible z-index:1">
+        <h3>Benchmarking</h3>
+      </div>
+      <div STYLE="position:absolute; top:320px; left:100px; visibility:visible z-index:1">
       <Link to="/Encuesta">
-      <IconButton edge="end" className={classes.BusinessIcon} color="inherit"  aria-label="menu">
-          <Button 
-            variant="contained"
-            color="inherit"
-            className={classes.button}
-            startIcon={<BusinessIcon />}
-            >
-            Pyme
-          </Button>
-        </IconButton>
+        <Button variant="contained">Realizar Consulta</Button>
       </Link>
+      </div>
+      <div STYLE="position:absolute; top:86px; left:950px; visibility:visible z-index:1">
       <Link to="/ABM">
-      <IconButton edge="end" className={classes.SupervisorAccountIcon} color="inherit"  aria-label="menu">
-          <Button 
-            variant="contained"
+          <Button
             color="inherit"
-            className={classes.button}
-            startIcon={<SupervisorAccountIcon />}
-            >
-            Administrador
-          </Button>
-        </IconButton>
+            startIcon={<LockIcon />}
+            >Admin</Button>
       </Link>
-      </p>
+      </div>
       <div id="logos">
         <IconButton edge="start" className={classes.InstagramIcon} color="inherit"  aria-label="menu" href="https://www.instagram.com/observatoriopyme/" target="_blank">
           <InstagramIcon/>
