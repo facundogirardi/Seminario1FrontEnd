c79787207de07797114a4299cf14e74b6adfcd0a
unknown <facundogirardi@gmail.com>
2021-05-12 17:42:16 -0300

    cambio de imagenes

--- src/components/Operador/Operador.js
+++ src/components/Operador/Operador.js
@@ -7,7 +7,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import EditableTable from "./TableEncuesta"
-import banner from '../../imagenes/banner3.jpg';
+import banner from '../../imagenes/banner1.jpg';
 
 //importo 
 import { getEncuesta } from "../../controller/miApp.controller";
